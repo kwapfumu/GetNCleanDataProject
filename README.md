3a52b11156711b7fc6f6c5818cd6e9a759ae8c97
Lilia Rigumye <rigumyel@gmail.com>
2014-10-26 21:29:33 +0100

    Creating README file and pushing run_analysis file

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-GetNCleanDataProject
-====================
+==================================================================================================
+Project submitted for the Getting and Cleaning Data course in Coursera Data Science specialization
+Oct 2014
+==================================================================================================
+1. In RStudio, Set your working directory to "UCI HAR Dataset" directory(the folder that contains all the raw datasets)
+2. Install "dplyr" and "reshape2" packages
+3. In the console type source("run_analysis.R") and then run_analysis()
 
-A repository created for the submission of the Getting and Cleaning Data course project
+##What run_analysis does
+###Loading features data
+1. reads into R the data in "features.txt" and stores it into a data.frame named "rawFeatures" with 561 observations of 2 variables
+2. renames "rawFeatures" columns into "featureNumber" and "feature"
+3. subsets the "feature" column from "rawFeature" data.frame and stores it into factor object named "features"
+
+###Loading test data
+4. reads into R the data in "X_test.txt" and stores it into a data.frame named "rawTestset" with 2947 observations of 561 variables
+5. renames "rawTestset" columns into corresponding features names using the "features" object
+6. reads into R the data in "y_test.txt" and stores it into a data.frame named "rawTestActivity" with 2947 observations of 1 variable
+7. renames "rawTestActivity" column into "Activity"
+8. reads into R the data in "subject_test.txt" and stores it into a data.frame named "rawSubjectsTest" with 2947 observations of 1 variable
+9. renames "rawSubjectsTest" column into "SubjectId"
+10. creates a data.frame with "SubjectId" and "Activity" columns named "subjectsNtestActivity"
+11. creates a "testData" data.frame with columns "SubjectId", "Activity", and the features names as remaining columns i.e 2947 observations of 563 variables
+
+###Loading train data
+12. reads into R the data in "X_train.txt" and stores it into a data.frame named "rawTrainingset" with 7352 observations of 561 variables
+13. renames "rawTrainingset" columns into corresponding features names using the "features" object
+14. reads into R the data in "y_train.txt" and stores it into a data.frame named "rawTrainingActivity" with 7352 observations of 1 variable
+15. renames "rawTrainingActivity" column into "Activity"
+16. reads into R the data in "subject_train.txt" and stores it into a data.frame named "rawSubjectsTrain" with 7352 observations of 1 variable
+17. renames "rawSubjectsTrain" column into "SubjectId"
+18. creates a data.frame with "SubjectId" and "Activity" columns named "subjectsNtrainActivity"
+19. creates a "trainData" data.frame with columns "SubjectId", "Activity", and the features names as remaining columns i.e 7352 observations of 563 variables
+
+###Merging trainData set with testData set
+20. merges "trainData" and "testData" into "processedDataSet" data.frame of 10299 observations of 563 variables
+
+###Extracting columns with mean and standard deviation in it
+21. extracts columns with "mean()", "mean()-X/Y/Z and meanFreq()-X/Y/Z from "processedDataSet" and storing them into "proDatasetwithmean" data.frame of 10299 observations of 46 variables
+22. installs and loads "dplyr"
+23. extracts columns with mean() and mean()-X/Y/Z only from "proDatasetwithmean" and stores them into "proDatasetwithmeanOnly" data.frame of 10299 observations of 33 variables
+24. extracts columns with std() and std()-X/Y/Z from "processedDataSet" and storing them into "proDatasetwithstd" data.frame of 10299 observations of 33 variables
+25. merges everything into one data.frame named "messyDataSet" data.frame of 10299 observations of 68 variables
+
+###Renaming Activity values
+26. changes "messyDataSet$Activity" values from "Integer" to "character" type
+27. replaces all "messyDataSet$Activity" values into corresponding activity's names
+
+###Restructuring and aggregating data set
+28. installs and loads "reshape" package
+29. restructures dataset in such way that each observation is in its own row and stores it into "tidyDataSet"
+30. aggregates the Dataset by subjectId and Activity, computes the mean for each numeric value and stores them into "averagedTidyDataSet"
+
+###Writing the dataset to a ".txt" file
+31. writes our "averagedTidyDataSet" to a "averagedTidyDataSet.txt" file inside the working directory
+
+###Printing the first 6 rows of the "averagedTidyDataSet" on the console
+32. prints the first 6 rows of the "averagedTidyDataSet" on the console
+
+   **_This is it, hope your reading was smooth..._**
---
+++ run_analysis.R
@@ -0,0 +1,89 @@
+#############################################################################################################################################
+##############script getting and cleaning data obtained from a samsung#######################################################################
+#############################################################################################################################################
+#############################################################################################################################################
+run_analysis <- function(){
+    ##################loading features data######################
+    #loading List of all features
+    rawFeatures <- read.table("features.txt", header= FALSE)
+    #renaming colnames
+    colnames(rawFeatures) <- c("featureNumber","feature")
+    features <- rawFeatures[,2]
+
+    #####loading test related data##############################
+    #loading Test set
+    rawTestset <- read.table("test/X_test.txt", header = FALSE)
+    #rename column names
+    colnames(rawTestset) <- features
+    #loading Test labels
+    rawTestActivity <- read.table("test/y_test.txt", header = FALSE)
+    #renaming rawTestActivity column
+    colnames(rawTestActivity) <- c("Activity")
+    #loading subjects data
+    #Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+    rawSubjectsTest <- read.table("test/subject_test.txt", header = FALSE)
+    #renaming rawSubjectsTest column
+    colnames(rawSubjectsTest) <- c("SubjectId")
+    #biding subjectId and Activity columns
+    subjectsNtestActivity <- cbind(rawSubjectsTest,rawTestActivity)
+    #creating the processed test data set
+    testData <- cbind(subjectsNtestActivity,rawTestset)
+
+    #####loading train data#########################################################################################
+    #loading training set
+    rawTrainingset <- read.table("train/X_train.txt", header = FALSE)
+    #rename column names
+    colnames(rawTrainingset) <- features
+    #loading training labels
+    rawTrainingActivity <- read.table("train/y_train.txt", header = FALSE)
+    colnames(rawTrainingActivity) <- c("Activity")
+    #loading subject data
+    #Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+    rawSubjectsTrain <- read.table("train/subject_train.txt", header = FALSE)
+    #renaming rawSubjectsTrain column
+    colnames(rawSubjectsTrain) <- c("SubjectId")
+    #biding subjectId and Activity columns
+    subjectsNtrainActivity <- cbind(rawSubjectsTrain,rawTrainingActivity)
+    #creating the processed train data set
+    trainData <- cbind(subjectsNtrainActivity,rawTrainingset)
+
+    #######################merging trainData set with testData set ########################################################
+    #merges trainData and testData
+    processedDataSet <- rbind(testData,trainData)
+
+    #######################extracting columns with mean and standard deviation in it #####################################
+    #extract columns with mean() , mean()-X/Y/Z and meanFreq()-X/Y/Z
+    proDatasetwithmean <- processedDataSet[,grepl("mean()", colnames(processedDataSet))]
+    #loads "dplyr"
+    library(dplyr)
+    #extract columns with mean() and mean()-X/Y/Z only
+    proDatasetwithmeanOnly <- select(proDatasetwithmean,-(c(24,25,26,30,31,32,36,37,38,40,42,44,46)))
+    #extract columns with std() and std()-X/Y/Z
+    proDatasetwithstd <- processedDataSet[,grepl("std()", colnames(processedDataSet))]
+    #merges everything into "messyDataSet"
+    messyDataSet <- cbind(processedDataSet[,1:2],proDatasetwithmeanOnly,proDatasetwithstd)
+
+    ##############################renaming Activity values##################################################################
+    messyDataSet$Activity <- as.character(messyDataSet$Activity)
+    messyDataSet$Activity[messyDataSet$Activity == "1"] <- "WALKING"
+    messyDataSet$Activity[messyDataSet$Activity == "2"] <- "WALKING_UPSTAIRS"
+    messyDataSet$Activity[messyDataSet$Activity == "3"] <- "WALKING_DOWNSTAIRS"
+    messyDataSet$Activity[messyDataSet$Activity == "4"] <- "SITTING"
+    messyDataSet$Activity[messyDataSet$Activity == "5"] <- "STANDING"
+    messyDataSet$Activity[messyDataSet$Activity == "6"] <- "LAYING"
+
+    ###################################################################
+    ###use reshape package to restructuring and aggregating dataset###
+    #################################################################
+    library(reshape2)
+    ##restructures dataset in such way that each observation is in its own row
+    tidyDataSet <- melt(messyDataSet,id=(c("SubjectId","Activity")))
+    #aggregates the Dataset by subjectId and Activity and computes the mean for each numeric value
+    averagedTidyDataSet <- cast(tidyDataSet,SubjectId+Activity~variable,mean)
+
+    #####################writes our tidyDataSet to a file "averagedTidyDataSet"######################
+    write.table(averagedTidyDataSet, "averagedTidyDataSet.txt",row.name=FALSE)
+
+    #####prints the first 6 rows of averagedTidyDataSet to the console#############
+    return(head(averagedTidyDataSet))
+}
